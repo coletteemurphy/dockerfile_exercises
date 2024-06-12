09d7179fc977f0c1567fe49efc182b1610aef608
colette <colette@test.com>
2024-06-12 09:54:54 +0100

    indentation errorrs

--- Task2/flask-app/app.py
+++ Task2/flask-app/app.py
@@ -15,11 +15,11 @@ class Users(db.Model):
  
     def __repr__(self): 
         return ''.join(['User ID: ', str(self.id), '\r\n', 'Email: ', self.email, ' Name: ', self.first_name, ' ', self.last_name, '\n']) 
-    @app.route('/') 
+@app.route('/') 
  
-    def hello(): 
-        data1 = Users.query.all() 
-        return render_template('home.html', data1=data1) 
+def hello(): 
+    data1 = Users.query.all() 
+    return render_template('home.html', data1=data1) 
  
-    if __name__=='__main__': 
+if __name__=='__main__': 
     app.run(host='0.0.0.0', port=5000, debug=True)
